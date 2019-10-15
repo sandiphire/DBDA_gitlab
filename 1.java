192eedfd5e0f5277e70dbe935c82ccc468e7b9fe
Your Name <sandip.hire2015@gmail.com>
2019-10-15 08:17:22 -0700

    shell file

---
+++ 1.java
@@ -0,0 +1,7 @@
+Class A
+{
+   public void static main(String args [])
+   {
+     System.out.println("Hello Everyone, welcome to java file");
+   }
+}
---
+++ 1.py
@@ -0,0 +1,1 @@
+print("Hello Everyone, Welcome to python file");
